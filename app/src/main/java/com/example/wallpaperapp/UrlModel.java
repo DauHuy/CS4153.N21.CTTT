5f425d9538674ffc08c92902a2bd7495090df98f
DauHuy <91195338+DauHuy@users.noreply.github.com>
2023-05-25 18:43:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/wallpaperapp/UrlModel.java
@@ -0,0 +1,18 @@
+package com.example.wallpaperapp;
+
+public class UrlModel {
+
+    private String portait;
+
+    public String getPortait() {
+        return portait;
+    }
+
+    public void setPortait(String portait) {
+        this.portait = portait;
+    }
+
+    public UrlModel(String portait) {
+        this.portait = portait;
+    }
+}
