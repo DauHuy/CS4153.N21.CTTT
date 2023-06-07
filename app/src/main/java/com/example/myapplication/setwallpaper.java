5d4e260670d0340806b51563a938c8601e37cf01
DauHuy <91195338+DauHuy@users.noreply.github.com>
2023-06-07 20:16:25 +0700

    commit
    add download figure

---
+++ app/src/main/java/com/example/myapplication/setwallpaper.java
@@ -0,0 +1,68 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DownloadManager;
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.io.IOException;
+
+public class setwallpaper extends AppCompatActivity {
+
+    Intent intent;
+    ImageView imageView;
+    Button set;
+    String url = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setwallpaper);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        final WallpaperManager wallpaperManager=WallpaperManager.getInstance(getApplicationContext());
+        set=findViewById(R.id.set);
+        imageView=findViewById(R.id.finalimage);
+        intent=getIntent();
+        String url=intent.getStringExtra("image");
+        Glide.with(getApplicationContext()).load(url).into(imageView);
+        set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    Bitmap bitmap=((BitmapDrawable)imageView.getDrawable()).getBitmap();
+                    wallpaperManager.setBitmap(bitmap);
+                    Toast.makeText(getApplicationContext(), "DONE",Toast.LENGTH_SHORT).show();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+    public void DownloadWallpaperEvent(View view) {
+
+        DownloadManager downloadManager = (DownloadManager)getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        downloadManager.enqueue(request);
+        Toast.makeText(this, "Downloading Start", Toast.LENGTH_SHORT).show();
+    }
+}
